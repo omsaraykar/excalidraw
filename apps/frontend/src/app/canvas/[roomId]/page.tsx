da361f530653a34fa4a17bdeaffbfa0533aa5a29
omsaraykar <omsaraykar@gmail.com>
2025-07-28 23:54:00 +0530

    use-params bug fixed

--- apps/frontend/src/app/canvas/[roomId]/page.tsx
+++ apps/frontend/src/app/canvas/[roomId]/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import RoomCanvas from "@/components/canvas/room-canvas";
 
-export default async function CanvasPage({ params }: { params: { roomId: string } }) { 
-  const roomId = await params.roomId;
+export default async function CanvasPage() { 
+  const params = useParams()
+  if (!params.roomId) {
+    params.roomId = "";
+  }
+  const roomId = params.roomId.toString();
   return (
     <RoomCanvas roomId={roomId} />
   );
